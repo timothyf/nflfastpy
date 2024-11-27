6633f9aa5502a532c6da93b1b1e971ceae042674
Timothy Fisher <timothyf@gmail.com>
2024-11-27 17:02:38 -0500

    Update config.py

--- nflfastpy/config.py
+++ nflfastpy/config.py
@@ -1,6 +1,7 @@
-BASE_URL = 'https://github.com/guga31bb/nflfastR-data/blob/master/data/play_by_play_{year}.csv.gz?raw=True'
-ROSTER_URL = 'https://raw.githubusercontent.com/nflverse/nflfastR-roster/master/data/seasons/roster_{year}.csv'
-DEPTH_CHART_URL = 'https://github.com/nflverse/nflfastR-roster/blob/master/data/seasons/depth_charts_{year}.csv.gz?raw=True'
-INJURY_URL = 'https://github.com/nflverse/nflfastR-roster/blob/master/data/seasons/injuries_{year}.csv.gz?raw=True'
-TEAM_LOGO_URL = 'https://raw.githubusercontent.com/guga31bb/nflfastR-data/master/teams_colors_logos.csv'
-SCHEDULE_URL = 'https://github.com/guga31bb/nflfastR-data/blob/master/schedules/sched_{year}.rds?raw=True'
+BASE_URL = 'https://github.com/nflverse/nflverse-data/releases/download/pbp/play_by_play_{year}.csv.gz'
+ROSTER_URL = 'https://github.com/nflverse/nflverse-data/releases/download/rosters/roster_{year}.csv'
+DEPTH_CHART_URL = 'https://github.com/nflverse/nflverse-data/releases/download/depth_charts/depth_charts_{year}.csv'
+INJURY_URL = 'https://github.com/nflverse/nflfastR-roster/blob/releases/download/seasons/injuries_{year}.csv.gz?raw=True'
+TEAM_LOGO_URL = 'https://raw.githubusercontent.com/nflverse/nfldata/refs/heads/master/data/teamcolors.csv'
+SCHEDULE_URL = 'https://github.com/nflverse/nflfastR-data/releases/download/schedules/sched_{year}.rds?raw=True'
+
